0324db32fe4c7fc7a8ffb86a538c0f5babbafbd3
CaurissaN <caurissanev@gmail.com>
2023-04-28 10:37:10 -0400

    set up basic layout to work from

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        messsage: "What is the title of this project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        messsage: "Give a brief summary of the project?"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        err ? console.log(err) : console.log("README.md was created!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("README.md", generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
 init();
